5d744f1c868299e3122b296a0a46c6db8fe1ff26
Embers-of-the-Fire <stellarishs@163.com>
2026-01-03 22:01:23 +0800

    add: improve sidebar top padding

--- packages/workstation/src/lib/components/Sidebar.svelte
+++ packages/workstation/src/lib/components/Sidebar.svelte
@@ -15,14 +15,17 @@
 </script>
 
 <SidebarButton breakpoint="lg" onclick={sidebarUi.toggle}
-               class="inline-flex items-center text-sm rounded-lg hover:bg-gray-100 focus:outline-hidden focus:ring-2 focus:ring-gray-200 dark:hover:bg-gray-700 dark:focus:ring-gray-600 lg:hidden fixed top-2 z-40 mb-2 md:top-4" />
+               class="inline-flex items-center text-sm rounded-lg
+                      hover:bg-gray-100 focus:outline-hidden focus:ring-2
+                      focus:ring-gray-200 dark:hover:bg-gray-700 dark:focus:ring-gray-600
+                      lg:hidden fixed top-2 z-40 mb-2 md:top-6" />
 <Sidebar
   breakpoint="lg"
   backdrop={false}
   {isOpen}
   closeSidebar={sidebarUi.close}
   params={{ x: -50, duration: 50 }}
-  class="top-0 left-0 mt-[61px] lg:mt-[94px] h-screen w-64 bg-gray-50 transition-transform lg:block dark:bg-gray-800 border-r border-gray-200"
+  class="top-0 left-0 mt-[61px] md:mt-[90px] lg:mt-[92px] h-screen w-64 bg-gray-50 transition-transform lg:block dark:bg-gray-800 border-r border-gray-200"
   classes={{ div: "h-full overflow-y-auto bg-gray-50 dark:bg-gray-800 p-0" }}
 >
   <h4 class="sr-only">Main Menu</h4>
